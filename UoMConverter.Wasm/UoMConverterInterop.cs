2fd42e52c8f3e9415991092094280201a77e5592
nullzerovibe <nullzerovibe@gmail.com>
2026-02-14 11:34:14 +0100

    - love vibes

--- UoMConverter.Wasm/UoMConverterInterop.cs
+++ UoMConverter.Wasm/UoMConverterInterop.cs
@@ -78,6 +78,15 @@ public class UoMConverterInterop {
     /// <returns>A JSON string containing documentation.</returns>
     [JSInvokable]
     public string GetDocumentation() => _converter.GetDocumentation();
+    /// <summary>
+    /// Retrieves the version of the assembly.
+    /// </summary>
+    /// <returns>The version string (e.g., "1.0.0.0").</returns>
+    [JSInvokable]
+    public string GetVersion()
+    {
+        return System.Reflection.Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "Unknown";
+    }
 }
 
 /// <summary>
